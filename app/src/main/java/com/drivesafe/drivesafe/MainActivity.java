d02409e36fd85a2a7bce243bdce6f95672499fba
Yossi Elman <yossi.elman@gmail.com>
2017-12-04 18:53:24 +0200

    get HR from band

--- app/src/main/java/com/drivesafe/drivesafe/MainActivity.java
+++ app/src/main/java/com/drivesafe/drivesafe/MainActivity.java
@@ -3,16 +3,31 @@ package com.drivesafe.drivesafe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.io.*;
+import java.lang.ref.WeakReference;
+
 import android.app.*;
 import android.content.*;
 import android.net.*;
-import android.os.*;
 import android.view.*;
 import android.graphics.*;
 import android.widget.*;
 import android.provider.*;
+
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandIOException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.ConnectionState;
+import com.microsoft.band.UserConsent;
 import com.microsoft.projectoxford.face.*;
 import com.microsoft.projectoxford.face.contract.*;
+
+import com.microsoft.band.BandClient;
+import com.microsoft.band.sensors.BandHeartRateEvent;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
+import com.microsoft.band.sensors.HeartRateConsentListener;
+import android.os.AsyncTask;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,23 +38,204 @@ public class MainActivity extends AppCompatActivity {
     private final String api_endpoint = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false";
     private FaceServiceClient faceServiceClient =
             new FaceServiceRestClient(api_endpoint, sub_key);
+    private BandClient client = null;
+    Button btnStart, btnConcent;
+    TextView hr_text;
+    Button button2 = (Button)findViewById(R.id.button_concent);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button1 = (Button)findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        btnStart = (Button)findViewById(R.id.button_hr);
+        btnConcent = (Button) findViewById(R.id.button_concent);
+        hr_text = (TextView) findViewById(R.id.HR_textView);
+        final WeakReference<Activity> reference = new WeakReference<Activity>(this);
+        btnConcent.setOnClickListener(new View.OnClickListener() {
+            @SuppressWarnings("unchecked")
             @Override
             public void onClick(View v) {
-                Intent gallIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                gallIntent.setType("image/*");
-                startActivityForResult(Intent.createChooser(gallIntent, "Select Picture"), PICK_IMAGE);
-            }
+                new HeartRateConsentTask().execute(reference);
 
+            }
         });
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hr_text.setText(" hi");
+                new HeartRateSubscriptionTask().execute();
+            }
+        });
+
         detectionProgressDialog = new ProgressDialog(this);
     }
+
+    //Need to get user consent
+    private class HeartRateConsentTask extends AsyncTask<WeakReference<Activity>, Void, Void> {
+        @Override
+        protected Void doInBackground(WeakReference<Activity>... params) {
+            try {
+                if (getConnectedBandClient()) {
+
+                    if (params[0].get() != null) {
+                        client.getSensorManager().requestHeartRateConsent(params[0].get(), new HeartRateConsentListener() {
+                            @Override
+                            public void userAccepted(boolean consentGiven) {
+                            }
+                        });
+                    }
+                } else {
+                    appendToUI("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
+                }
+            } catch (BandException e) {
+                String exceptionMessage="";
+                switch (e.getErrorType()) {
+                    case UNSUPPORTED_SDK_VERSION_ERROR:
+                        exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
+                        break;
+                    case SERVICE_ERROR:
+                        exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
+                        break;
+                    default:
+                        exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
+                        break;
+                }
+                appendToUI(exceptionMessage);
+
+            } catch (Exception e) {
+                appendToUI(e.getMessage());
+            }
+            return null;
+        }
+    }
+
+
+    private BandHeartRateEventListener mHeartRateEventListener = new BandHeartRateEventListener() {
+        @Override
+        public void onBandHeartRateChanged(final BandHeartRateEvent event) {
+            if (event != null)
+            {//update screen
+                TextView hr_text = (TextView)findViewById(R.id.HR_textView);
+                hr_text.setText(String.format("Heart Rate = %d beats per minute\n"
+                        + "Quality = %s\n", event.getHeartRate(), event.getQuality()));
+            }
+        }
+    };
+
+    //Kick off the heart rate reading
+    private class HeartRateSubscriptionTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                if (getConnectedBandClient()) {
+                    if (client.getSensorManager().getCurrentHeartRateConsent() == UserConsent.GRANTED) {
+                        client.getSensorManager().registerHeartRateEventListener(mHeartRateEventListener);
+                    } else {
+                        appendToUI("You have not given this application consent to access heart rate data yet."
+                                + " Please press the Heart Rate Consent button.\n");
+                    }
+                } else {
+                    appendToUI("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
+                }
+            } catch (BandException e) {
+                String exceptionMessage="";
+                switch (e.getErrorType()) {
+                    case UNSUPPORTED_SDK_VERSION_ERROR:
+                        exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
+                        break;
+                    case SERVICE_ERROR:
+                        exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
+                        break;
+                    default:
+                        exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
+                        break;
+                }
+                appendToUI(exceptionMessage);
+
+            } catch (Exception e) {
+                appendToUI(e.getMessage());
+            }
+            return null;
+        }
+    }
+
+    //Get connection to band
+    private boolean getConnectedBandClient() throws InterruptedException, BandException {
+
+        if (client == null) {
+            //Find paired bands
+            BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
+            if (devices.length == 0) {
+                //No bands found...message to user
+                return false;
+            }
+            //need to set client if there are devices
+            client = BandClientManager.getInstance().create(getBaseContext(), devices[0]);
+        } else if(ConnectionState.CONNECTED == client.getConnectionState()) {
+            return true;
+        }
+
+        //need to return connected status
+        return ConnectionState.CONNECTED == client.connect().await();
+    }
+
+    private void appendToUI(final String string) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                hr_text.setText(string);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hr_text.setText("");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (client != null) {
+            try {
+                client.getSensorManager().unregisterHeartRateEventListener(mHeartRateEventListener);
+            } catch (BandIOException e) {
+                appendToUI(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (client != null) {
+            try {
+                client.disconnect().await();
+            } catch (InterruptedException e) {
+                // Do nothing as this is happening during destroy
+            } catch (BandException e) {
+                // Do nothing as this is happening during destroy
+            }
+        }
+        super.onDestroy();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
