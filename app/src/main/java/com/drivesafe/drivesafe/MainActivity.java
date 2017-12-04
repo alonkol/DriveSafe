11246cb4931bc664ee93e92f9144f95b338c3ac4
alonkol <alonkollmann@gmail.com>
2017-12-04 18:03:42 +0200

    Added eye closure calculations and removed unnecessary code

--- app/src/main/java/com/drivesafe/drivesafe/MainActivity.java
+++ app/src/main/java/com/drivesafe/drivesafe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.drivesafe.drivesafe;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.io.*;
@@ -11,18 +12,35 @@ import android.view.*;
 import android.graphics.*;
 import android.widget.*;
 import android.provider.*;
-import com.microsoft.projectoxford.face.*;
-import com.microsoft.projectoxford.face.contract.*;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.microsoft.projectoxford.face.*;
+import com.microsoft.projectoxford.face.common.RequestMethod;
+import com.microsoft.projectoxford.face.contract.*;
+import com.microsoft.projectoxford.face.rest.ClientException;
+import com.microsoft.projectoxford.face.rest.WebServiceRequest;
+
+
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int PICK_IMAGE = 1;
     private ProgressDialog detectionProgressDialog;
     private final String sub_key = "f828f841dd4642249e6c9fcd69784bed";
-    private final String api_endpoint = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false";
-    private FaceServiceClient faceServiceClient =
-            new FaceServiceRestClient(api_endpoint, sub_key);
+    private final String api_endpoint = "https://westeurope.api.cognitive.microsoft.com/face/v1.0";
+
+    // copied from FaceServiceRestClient
+    private final WebServiceRequest mRestCall = new WebServiceRequest(sub_key);
+    private Gson mGson = (new GsonBuilder()).setDateFormat("M/d/yyyy h:m:s a").create();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
 
         });
         detectionProgressDialog = new ProgressDialog(this);
+
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -57,85 +77,88 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Detect faces by uploading face images
-// Frame faces after detection
+    // Frame faces after detection
 
     private void detectAndFrame(final Bitmap imageBitmap)
     {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
-        ByteArrayInputStream inputStream =
-                new ByteArrayInputStream(outputStream.toByteArray());
-        AsyncTask<InputStream, String, Face[]> detectTask =
-                new AsyncTask<InputStream, String, Face[]>() {
+
+        AsyncTask<ByteArrayOutputStream, String, Face[]> detectTask =
+                new AsyncTask<ByteArrayOutputStream, String, Face[]>() {
                     @Override
-                    protected Face[] doInBackground(InputStream... params) {
+                    protected Face[] doInBackground(ByteArrayOutputStream... params) {
+                        Face[] result = new Face[]{};
                         try {
-                            publishProgress("Detecting...");
-                            Face[] result = faceServiceClient.detect(
-                                    params[0],
+                            result = myDetect(
+                                    params[0], // image stream
                                     true,         // returnFaceId
-                                    false,        // returnFaceLandmarks
-                                    null           // returnFaceAttributes: a string like "age, gender"
+                                    true,        // returnFaceLandmarks
+                                    "age,occlusion"           // returnFaceAttributes: a string like "age, gender"
                             );
                             if (result == null)
                             {
-                                publishProgress("Detection Finished. Nothing detected");
-                                return null;
+                                // TODO
                             }
-                            publishProgress(
-                                    String.format("Detection Finished. %d face(s) detected",
-                                            result.length));
-                            return result;
+                            FaceLandmarks landmarks = result[0].faceLandmarks;
+                            // TODO: change to sqrt(dx^2 + dy^2). Currently vertical only
+                            // TODO: why is eyeLeftBottom higher than eyeLeftTop???
+                            double occlusionLeft = landmarks.eyeLeftTop.y - landmarks.eyeLeftBottom.y;
+                            double occlusionRight = landmarks.eyeRightTop.y - landmarks.eyeRightBottom.y;
+                            double averageOcclusion = (occlusionLeft + occlusionRight) / 2;
+
                         } catch (Exception e) {
-                            publishProgress("Detection failed");
-                            return null;
+                            // TODO
                         }
-                    }
-                    @Override
-                    protected void onPreExecute() {
 
-                        detectionProgressDialog.show();
-                    }
-                    @Override
-                    protected void onProgressUpdate(String... progress) {
-
-                        detectionProgressDialog.setMessage(progress[0]);
-                    }
-                    @Override
-                    protected void onPostExecute(Face[] result) {
-
-                        detectionProgressDialog.dismiss();
-                        if (result == null) return;
-                        ImageView imageView = (ImageView)findViewById(R.id.imageView1);
-                        imageView.setImageBitmap(drawFaceRectanglesOnBitmap(imageBitmap, result));
-                        imageBitmap.recycle();
+                        return result;
                     }
                 };
-        detectTask.execute(inputStream);
+        detectTask.execute(outputStream);
+
+
     }
 
-    private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces) {
-        Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
-        Canvas canvas = new Canvas(bitmap);
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.RED);
-        int stokeWidth = 2;
-        paint.setStrokeWidth(stokeWidth);
-        if (faces != null) {
-            for (Face face : faces) {
-                FaceRectangle faceRectangle = face.faceRectangle;
-                canvas.drawRect(
-                        faceRectangle.left,
-                        faceRectangle.top,
-                        faceRectangle.left + faceRectangle.width,
-                        faceRectangle.top + faceRectangle.height,
-                        paint);
-            }
-        }
-        return bitmap;
-    }
+    public Face[] myDetect(ByteArrayOutputStream byteArrayOutputStream, boolean returnFaceId, boolean returnFaceLandmarks, String returnFaceAttributes) throws ClientException, IOException {
+        Map<String, Object> params = new HashMap();
+        params.put("returnFaceId", Boolean.valueOf(returnFaceId));
+        params.put("returnFaceLandmarks", Boolean.valueOf(returnFaceLandmarks));
+        params.put("returnFaceAttributes", returnFaceAttributes);
 
+        String path = String.format("%s/%s", new Object[]{this.api_endpoint, "detect"});
+        String uri = WebServiceRequest.getUrl(path, params);
+
+        byte[] data = byteArrayOutputStream.toByteArray();
+        params.clear();
+        params.put("data", data);
+        String json = (String)this.mRestCall.request(uri, RequestMethod.POST, params, "application/octet-stream");
+        Type listType = (new TypeToken<List<Face>>() {
+        }).getType();
+        List<Face> faces = (List)this.mGson.fromJson(json, listType);
+        return faces.toArray(new Face[faces.size()]);
+    }
 
 }
+
+class Face {
+    public UUID faceId;
+    public FaceRectangle faceRectangle;
+    public FaceLandmarks faceLandmarks;
+    public FaceAttribute faceAttributes;
+
+    public Face() {
+    }
+}
+
+// TODO: if necessary, add property for Occlusion etc.
+class FaceAttribute {
+    public double age;
+    public String gender;
+    public double smile;
+    public FacialHair facialHair;
+    public HeadPose headPose;
+
+    public FaceAttribute() {
+    }
+}
+
