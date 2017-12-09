dbc79a11fe047bd8ed280eb47489782fbc786fb4
noammansur <noam@optibus.co>
2017-12-09 18:56:29 +0200

    wip video showing

---
+++ app/src/main/java/com/drivesafe/drivesafe/ImageSurfaceView.java
@@ -0,0 +1,43 @@
+package com.drivesafe.drivesafe;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class ImageSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
+
+    public ImageSurfaceView(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        this.surfaceHolder = getHolder();
+        this.surfaceHolder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            this.camera.setPreviewDisplay(holder);
+            this.camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        this.camera.stopPreview();
+        this.camera.release();
+    }
+}
