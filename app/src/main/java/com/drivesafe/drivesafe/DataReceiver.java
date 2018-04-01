b5e4519e18128426e44521f24df219b2f3b40755
alonkol <alonkollmann@gmail.com>
2018-04-01 12:39:49 +0300

    small tweaks

--- app/src/main/java/com/drivesafe/drivesafe/DataReceiver.java
+++ app/src/main/java/com/drivesafe/drivesafe/DataReceiver.java
@@ -18,9 +18,8 @@ class DataReceiver extends Thread {
     private static String TAG = "DataReceiverTask";
     private final static String get24HoursAlertsNumber = "https://api.applicationinsights.io/v1/apps/ae986fd1-75ce-4773-b966-c364d6802517/metrics/customMetrics%2FAlert?timespan=P1D&interval=P1D&aggregation=count&top=1";
     private final static String get1HourAlertsNumber = "https://api.applicationinsights.io/v1/apps/ae986fd1-75ce-4773-b966-c364d6802517/metrics/customMetrics%2FAlert?timespan=PT1H&interval=PT1H&aggregation=count&top=1";
-    public static final MediaType JSON = MediaType.parse("application/json");
-    OkHttpClient client = null;
-    MainActivity mainActivity;
+    private OkHttpClient client = null;
+    private MainActivity mainActivity;
 
     private final double HIGH_RISK_THRESHOLD = 1.0/12;
 
@@ -38,13 +37,13 @@ class DataReceiver extends Thread {
     }
 
     public double getRiskScore() {
-        double last24HoursAlerts = (double) getLast24HoursAlerts();
-        double lastHourAlerts = (double) getLastHourAlerts();
+        double last24HoursAlerts = getLast24HoursAlerts();
+        double lastHourAlerts = getLastHourAlerts();
         if (last24HoursAlerts == 0)
         {
             return 0;
         }
-        return (double)lastHourAlerts/ last24HoursAlerts;
+        return lastHourAlerts / last24HoursAlerts;
     }
 
     private double getLastHourAlerts() {
@@ -58,14 +57,13 @@ class DataReceiver extends Thread {
     }
 
     private int getNumOfAlertsFromJson(JSONObject json) {
-        JSONArray arr = null;
         int alerts = 0;
         if (json == null)
         {
             return 0;
         }
         try {
-            arr = json.getJSONObject("value").getJSONArray("segments");
+            JSONArray arr = json.getJSONObject("value").getJSONArray("segments");
             for (int i = 0; i < arr.length(); i++)
             {
                 alerts += Integer.parseInt(arr.getJSONObject(i).getJSONObject("customMetrics/Alert").getString("count"));
@@ -77,30 +75,20 @@ class DataReceiver extends Thread {
     }
 
     private JSONObject sendApiRequest(String apiUrl ) {
-
         Request request = new Request.Builder()
                 .url(apiUrl)
                 .addHeader("x-api-key", "f6mfimdhku0fqg8xlslmafkuqd82xo0hjepvgzsh")
                 .build();
-        okhttp3.Response response = null;
         try {
-            response = client.newCall(request).execute();
+            okhttp3.Response response = client.newCall(request).execute();
             String responseData = response.body().string();
             JSONObject jsonObject = new JSONObject(responseData);
             Log.i(TAG, responseData);
             return jsonObject;
-        } catch (IOException e1) {
-            e1.printStackTrace();
+        } catch (IOException | NetworkOnMainThreadException | JSONException e) {
+            e.printStackTrace();
             return null;
         }
-        catch (NetworkOnMainThreadException e2) {
-            e2.printStackTrace();
-            return null;
-        }
-         catch (JSONException e3) {
-             e3.printStackTrace();
-             return null;
-         }
     }
 
 }
